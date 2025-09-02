9d60eff713dd87c7ce8e0f7aa78684ed26f05812
Gianluca-pul <gphgian@gmail.com>
2025-09-01 23:01:54 -0300

    Fetch a JSON

--- js/script.js
+++ js/script.js
@@ -18,4 +18,10 @@ function showData(dataArray) {
   }
 }
 
-// Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+// Fetch al JSON
+fetch(DATA_URL)
+  .then(response => response.json())
+  .then(data => {
+    showData(data.students); // Mostramos el array de estudiantes
+  })
+  .catch(error => console.error("Error al cargar los datos:", error));
